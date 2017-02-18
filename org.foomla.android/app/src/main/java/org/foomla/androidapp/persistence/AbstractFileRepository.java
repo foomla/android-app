076a52236054c42a979e90b51a7c9856ac419eff
Ingo Weinzierl <weinzierl.ingo@gmail.com>
2017-02-18 15:18:43 +0100

    Refactoring continues.....

--- org.foomla.android/app/src/main/java/org/foomla/androidapp/persistence/AbstractFileRepository.java
+++ org.foomla.android/app/src/main/java/org/foomla/androidapp/persistence/AbstractFileRepository.java
@@ -3,10 +3,14 @@ package org.foomla.androidapp.persistence;
 
 import com.google.gson.Gson;
 
+import org.apache.commons.io.IOUtils;
+
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.Serializable;
 
 public abstract class AbstractFileRepository<T extends Serializable> implements Repository<T> {
@@ -14,10 +18,14 @@ public abstract class AbstractFileRepository<T extends Serializable> implements 
     private Gson gson = new Gson();
 
     protected Object convert(File file, Class target) throws IOException {
-        return gson.fromJson(new FileReader(file), target);
+        String s = IOUtils.toString(new InputStreamReader(new FileInputStream(file)));
+        return gson.fromJson(s, target);
     }
 
     protected void write(File destination, T entity) throws IOException {
-        gson.toJson(entity, new FileWriter(destination));
+        final String jsonString = gson.toJson(entity);
+        OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(destination, false));
+        out.write(jsonString);
+        out.close();
     }
 }
