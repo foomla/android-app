838ad153d1ec510da98f6bacb5434fe6bf4b5f73
Ingo Weinzierl <weinzierl.ingo@gmail.com>
2017-08-28 22:14:13 +0200

    Fixed broken thumbnail loading.

--- org.foomla.android/app/src/main/java/org/foomla/androidapp/utils/ImageUtil.java
+++ org.foomla.android/app/src/main/java/org/foomla/androidapp/utils/ImageUtil.java
@@ -5,6 +5,8 @@ import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.google.common.base.Strings;
+
 import org.foomla.androidapp.R;
 import org.foomla.androidapp.domain.Exercise;
 import org.foomla.androidapp.domain.Training;
@@ -18,29 +20,14 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ImageUtil {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ImageUtil.class);
-
     private static final Map<String, Drawable> imageCache = new HashMap<>(125);
-
-    public enum ImageType {
-
-        NORMAL("exercise_%s.png"),
-        THUMBNAIL("exercise_%s_thumb.png"),
-        X_THUMBNAIL("exercise_%s_xthumb.png");
-
-        private final String imageName;
-
-        ImageType(final String imageName) {
-            this.imageName = imageName;
-        }
-
-        public String getImageFileName(final int exerciseId) {
-            return String.format(imageName, exerciseId);
-        }
-    }
+    private static final Pattern EXERCISE_ID_PATTERN = Pattern.compile(".*exercise_(\\d+)\\.png");
 
     public static Drawable createImageFromUrl(final String urlPath) {
         Object content;
@@ -67,10 +54,10 @@ public class ImageUtil {
             drawable = getLocalImage(context, training, exercise, imageType);
 
             if (drawable == null) {
-                drawable = getRemoteImage(exercise);
+                drawable = getRemoteImage(context, exercise, imageType);
             }
         } else {
-            drawable = getRemoteImage(exercise);
+            drawable = getRemoteImage(context, exercise, imageType);
         }
 
         return drawable;
@@ -85,6 +72,11 @@ public class ImageUtil {
         String imageName = imageType.getImageFileName(exercise.getId());
         String imageUrl = context.getString(R.string.exercise_image_url);
         return imageUrl + imageName;
+    }
+
+    private static String getImageUrl(final Context context, final int exerciseId, final ImageType imageType) {
+        return context.getString(R.string.exercise_image_url)
+                + imageType.getImageFileName(exerciseId);
     }
 
     private static Drawable getLocalImage(final Context context, final Training training, final Exercise exercise,
@@ -103,28 +95,50 @@ public class ImageUtil {
         return null;
     }
 
-    private static Drawable getRemoteImage(final Exercise exercise) {
+    private static Drawable getRemoteImage(final Context context, final Exercise exercise, ImageType imageType) {
         if (exercise.getImages() == null || exercise.getImages().isEmpty()) {
             return null;
         }
 
-        final String imageUrl = exercise.getImages().get(0);
-        Drawable image = imageCache.get(imageUrl);
+        final String exerciseImageUrl = exercise.getImages().get(0);
 
-        if (image == null) {
-            LOGGER.info("Image not found in cache -> load from remote location: {}", imageUrl);
+        try {
+            final int exerciseId = extractIdFromImageUrl(exerciseImageUrl);
+            final String imageUrl = getImageUrl(context, exerciseId, imageType);
 
-            image = createImageFromUrl(imageUrl);
-            imageCache.put(imageUrl, image);
-        } else {
-            LOGGER.info("Image found in cache: {}", imageUrl);
+            Drawable image = imageCache.get(imageUrl);
+
+            if (image == null) {
+                LOGGER.info("Image not found in cache -> load from remote location: {}", imageUrl);
+
+                image = createImageFromUrl(imageUrl);
+                imageCache.put(imageUrl, image);
+            } else {
+                LOGGER.info("Image found in cache: {}", imageUrl);
+            }
+
+            return image;
+        } catch (ExerciseIdNotFoundException e) {
+            LOGGER.error("Did not find exercise id from image url: {}", exerciseImageUrl);
+            return null;
         }
-
-        return image;
     }
 
     private static File getImageFile(final Exercise exercise, final ImageType imageType, final File imageDirectory) {
         return new File(imageDirectory, imageType.getImageFileName(exercise.getId()));
+    }
+
+    private static int extractIdFromImageUrl(String imageUrl) throws ExerciseIdNotFoundException {
+        if (imageUrl == null || Strings.isNullOrEmpty(imageUrl)) {
+            throw new ExerciseIdNotFoundException();
+        }
+
+        Matcher matcher = EXERCISE_ID_PATTERN.matcher(imageUrl);
+        if (matcher.matches() && matcher.groupCount() >= 1) {
+            return Integer.parseInt(matcher.group(1));
+        }
+
+        throw new ExerciseIdNotFoundException();
     }
 
     public static void calculateImageViewBounds(final ImageView imageView, final Drawable drawable) {
@@ -132,5 +146,25 @@ public class ImageUtil {
         float i = ((float) currentWidth) / ((float) drawable.getIntrinsicWidth());
         float imageHeight = i * (drawable.getIntrinsicHeight());
         imageView.setLayoutParams(new LinearLayout.LayoutParams(currentWidth, (int) imageHeight));
+    }
+
+    public enum ImageType {
+
+        NORMAL("exercise_%s.png"),
+        THUMBNAIL("exercise_%s_thumb.png"),
+        X_THUMBNAIL("exercise_%s_xthumb.png");
+
+        private final String imageName;
+
+        ImageType(final String imageName) {
+            this.imageName = imageName;
+        }
+
+        public String getImageFileName(final int exerciseId) {
+            return String.format(imageName, exerciseId);
+        }
+    }
+
+    private static class ExerciseIdNotFoundException extends Exception {
     }
 }
